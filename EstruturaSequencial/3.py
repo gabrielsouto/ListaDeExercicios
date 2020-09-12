23976148e0644f49cf85faf68d20743c010cbff9
gabrielsouto <emaildogabrielsouto@gmail.com>
2020-09-12 00:04:17 -0300

    Create 3.py

---
+++ EstruturaSequencial/3.py
@@ -0,0 +1,4 @@
+# 3. Faça um Programa que peça dois números e imprima a soma.
+numero1 = int(input("Digite o primeiro número:"))
+numero2 = int(input("Digite o segundo número:"))
+print("A soma de", numero1, "com", numero2, "é:", numero1+numero2)
