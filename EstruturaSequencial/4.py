cc2aed903552352cb2083ca3943a7a7993341a87
gabrielsouto <emaildogabrielsouto@gmail.com>
2020-09-12 22:45:51 -0300

    Create 4.py

---
+++ EstruturaSequencial/4.py
@@ -0,0 +1,6 @@
+# 4. Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+nota4 = float(input("Digite a quarta nota: "))
+print("A média das notas é:", (nota1+nota2+nota3+nota4)/4)
