a8df912bab40577d222931e76c1ccc4718ec453e
gabrielsouto <emaildogabrielsouto@gmail.com>
2020-09-14 22:17:49 -0300

    Delete 6.py

--- EstruturaSequencial/6.py
+++
@@ -1,5 +0,0 @@
-# 6. Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
-raio = float(input("Informe o raio do círculo: "))
-pi = 3.14159265359
-area =  pi * (raio**2)
-print("A área do círculo de raio ", raio, " é ", area)
