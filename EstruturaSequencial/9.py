55f9f1d265d815b5cf7eb9b9cb0a587aa2e73d4f
gabrielsouto <emaildogabrielsouto@gmail.com>
2020-09-16 22:43:41 -0300

    Create 9.py

---
+++ EstruturaSequencial/9.py
@@ -0,0 +1,3 @@
+# 9. Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius. C = 5 * ((F-32) / 9).
+temperatura_f = float(input("Digite a temperatura em Fahrenheit: "))
+print("A temperatura em Celsius é: ", round(5*((temperatura_f-32)/9), 2))
