3ade07c7d62254903bdf05e70c176b7424a303f5
gabrielsouto <emaildogabrielsouto@gmail.com>
2020-09-23 22:43:34 -0300

    Create 10.py

---
+++ ExerciciosListas/10.py
@@ -0,0 +1,17 @@
+# 10. Faça um Programa que leia dois vetores com 10 elementos cada. Gere um terceiro vetor de 20 elementos,
+# cujos valores deverão ser compostos pelos elementos intercalados dos dois outros vetores.
+primeiro_vetor = []
+segundo_vetor = []
+terceiro_vetor = []
+
+for posicao in range(10):
+    primeiro_vetor.append(input("Digite o elemento da "+str(posicao+1)+"ª posição do primeiro vetor: "))
+
+for posicao in range(10):
+    segundo_vetor.append(input("Digite o elemento da "+str(posicao+1)+"ª posição do segundo vetor: "))
+    
+for indice in range(10):
+    terceiro_vetor.append(primeiro_vetor[indice])
+    terceiro_vetor.append(segundo_vetor[indice])
+    
+print(terceiro_vetor)
