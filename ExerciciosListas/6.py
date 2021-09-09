936faaaf8f0dc0d52a94197dd5e660c1c1435cd9
Gabriel Souto <emaildogabrielsouto@gmail.com>
2021-09-09 12:59:03 -0300

    Update 6.py

--- ExerciciosListas/6.py
+++ ExerciciosListas/6.py
@@ -1,4 +1,4 @@
-# 6. Faça um Programa que peça as quatro notas de 10 alunos, alcule e armazene num vetor a média de cada aluno,
+# 6. Faça um Programa que peça as quatro notas de 10 alunos, calcule e armazene num vetor a média de cada aluno,
 # imprima o número de alunos com média maior ou igual a 7.0.
 
 medias_alunos = []
